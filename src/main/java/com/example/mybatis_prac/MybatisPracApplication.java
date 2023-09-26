22233249069ae698efd9935b6b61478e91fdd26d
Minwoo <made_power@naver.com>
2023-09-27 06:15:16 +0900

    Initial commit

---
+++ src/main/java/com/example/mybatis_prac/MybatisPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatis_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPracApplication.class, args);
+    }
+
+}
